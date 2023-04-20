9be43526dbfba14604d15f6b5d0acbeeaa6fe7a1
Yonin408 <justin12689@gmail.com>
2023-04-20 00:09:32 -0700

    dayList with map

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,22 @@
-import react from 'react';
+import React from 'react';
 import classNames from "classnames";
+import DayListItem from './DayListItem'
 
 export default function DayList(props) {
-
-    return (
+    const dayList = props.days.map((day) => {
+        return (
+            <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            selected={day.name === day}
+            setDay={props.setDay}
+            />   
+        )
+    })
+    return(
         <ul>
-
+            {dayList}
         </ul>
-    )
-}
+      )
+    }
