25eb5f917f3f9970e985c962e56bbe0027062d84
Yonin408 <justin12689@gmail.com>
2023-04-21 17:03:24 -0700

    Appointment functions to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,11 +2,50 @@ import React, { useState } from "react";
 import DayList from "./DayList";
 import DayListItem from "./DayListItem";
 import Button from "./Button";
+import Appointment from './Appointment/index'
 
 import "components/Application.scss";
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer:{
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer:{
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
 const days = [
     {
       id: 1,
@@ -45,8 +84,13 @@ onChange={setDay}
   alt="Lighthouse Labs"
 />
       </section>
-      <section className="schedule">
-
+      <section className="schedule" >
+     {Object.values(appointments).map((appointment) => (
+     <Appointment 
+  key={appointment.id} 
+  {...appointment} 
+/>))}
+     <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
