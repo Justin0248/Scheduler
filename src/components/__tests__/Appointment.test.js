102190b4cdaad32258655af583c4496ff996baaa
Yonin408 <justin12689@gmail.com>
2023-04-29 20:34:46 -0700

    cypress tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,7 +5,9 @@ import { render, cleanup } from "@testing-library/react";
 import Appointment from "../Appointment/index";
 
 afterEach(cleanup);
+describe("Application", () => {
 
 it("renders without crashing", () => {
-  render(<Application />);
+  render(<Appointment />);
+});
 });
